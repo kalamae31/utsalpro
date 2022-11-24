6d42ce2db09e3ec37cd64ef7d8cda55ee713a839
kalamae31 <115127502+kalamae31@users.noreply.github.com>
2022-11-24 12:55:30 +0700

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args) {
+        String nama = "Muhammad Jiddan kalam";
+        int usia = 2003;
+        
+        System.out.println("assalamualaikum");
+        System.out.println("Nama saya :"+ nama);
+        System.out.println("Saya lahir pada tahun :"+ usia);
+    }
+}
